249d74d206dcbffa908fbc5a6b00be94e7cfce8c
arun799raj-hue <arun799raj@gmail.com>
2026-01-31 22:31:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ st.set_page_config(page_title="AI Accident Report Generator", layout="centered")
 st.caption("Generate prfessional industrial accident reports using AI")
 genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 model = genai.GenerativeModel("models/gemini-1.5-flash")
-response = model.generate_content(prompt)
-report = response.text
 with st.form("accident_form"):
     st.subheader("Accident Details")
     worker_name = st.text_input("Worker Name")
